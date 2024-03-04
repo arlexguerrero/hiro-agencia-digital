05fd094d02c581ef36abfa6508c9209dd28b5397
sergio Guerrero <sergioarlex@gmail.com>
2024-03-03 19:58:08 -0500

    se añadieron botones de ir arriba y whatsapp

--- js/app.js
+++ js/app.js
@@ -42,3 +42,20 @@ $(document).ready(function(){
         }
     })
   });
+
+  // Detecta el desplazamiento de la página
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+}
+
+// Vuelve arriba al hacer clic en el botón
+document.getElementById("back-to-top").addEventListener("click", function() {
+    document.body.scrollTop = 0; // Para Safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+});
